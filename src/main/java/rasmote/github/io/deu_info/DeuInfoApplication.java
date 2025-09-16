a02efb1c9759778511e64e52eefa0ecfcb8297e8
Rasmote <workrasmote@gmail.com>
2025-09-16 10:25:46 +0900

    Initial commit

---
+++ src/main/java/rasmote/github/io/deu_info/DeuInfoApplication.java
@@ -0,0 +1,13 @@
+package rasmote.github.io.deu_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeuInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeuInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rasmote/github/io/deu_info/DeuInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package rasmote.github.io.deu_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeuInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
